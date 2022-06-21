4c7f01696b6d288b408953ffdc3dfb90c14d8980
Rômullo Cordeiro <romullocordeiro@gmail.com>
2022-06-21 20:17:21 -0300

    feat: login form molecule

---
+++ components/Molecules/LoginForm/index.tsx
@@ -0,0 +1,51 @@
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+
+import Button from '../../atoms/Button';
+import Checkbox from '../../atoms/Checkbox';
+import Input from '../../atoms/Input';
+
+const LoginForm = () => {
+  const FormSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, 'Mínimo de 3 carácteres')
+      .matches(
+        /^([a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð '])+$/u,
+        'Apenas letras permitidas'
+      ),
+  });
+  return (
+    <div>
+      <p className='text-sm'>Please, enter your full name below</p>
+      <p className='text-sm pt-4'>Only alphabetical characters are accepted</p>
+
+      <Formik
+        validationSchema={FormSchema}
+        initialValues={{ name: '', isAdult: false }}
+        onSubmit={() => {
+          alert('submit');
+        }}
+      >
+        {({ values, errors }) => (
+          <Form className='flex flex-col justify-between items-center'>
+            <div className='w-full py-4'>
+              <Input name='name' />
+            </div>
+            <div className='flex items-center text-sm pb-4'>
+              <Checkbox name='isAdult' />{' '}
+              <span>Are you older than 18 years old?</span>
+            </div>
+            <Button
+              type={'submit'}
+              label='Enter'
+              disabled={!values.isAdult || values.name.length < 3}
+              //   onClick={() => {}}
+            />
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default LoginForm;
