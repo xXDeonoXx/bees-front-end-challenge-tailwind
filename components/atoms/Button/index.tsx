5ea27821c6bfa1e077988e2e94e1966db9e857ac
Rômullo Cordeiro <romullocordeiro@gmail.com>
2022-06-21 20:16:54 -0300

    feat: button component with formik integration

--- components/atoms/Button/index.tsx
+++ components/atoms/Button/index.tsx
@@ -2,16 +2,27 @@ import React from 'react';
 
 interface ButtonProps {
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
   disabled?: boolean;
+  type?: 'button' | 'submit' | 'reset';
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick, disabled }) => {
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick,
+  disabled = false,
+  type = 'button',
+}) => {
   return (
     <button
+      disabled={disabled}
       className={`w-auto h-10 ${
         disabled ? 'bg-gray-600' : 'bg-iris-100'
       } text-white py-2 px-4 rounded`}
+      type={type}
+      onClick={() => {
+        onClick && onClick();
+      }}
     >
       {label}
     </button>
