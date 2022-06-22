38fd4ec2b84686994b4ed39fbac0c4e254bbb222
Rômullo Cordeiro <romullocordeiro@gmail.com>
2022-06-21 23:11:53 -0300

    feat: dynamic brewery list with real information consumption from api

--- components/Organisms/BreweryCard/index.tsx
+++ components/Organisms/BreweryCard/index.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import CardChip from '../../atoms/CardChip';
 import {
   ChartSquareBarIcon,
   LocationMarkerIcon,
   PhoneIcon,
   PlusCircleIcon,
 } from '@heroicons/react/outline';
+import { Brewery } from '../../../services/breweries/breweries.interface';
+import CardChip from '../../atoms/CardChip';
 
-const BreweryCard = () => {
+interface BreweryCardProps {
+  brewery: Brewery;
+}
+
+const BreweryCard: React.FC<BreweryCardProps> = ({ brewery }) => {
+  const address =
+    brewery.street || brewery.address_2 || brewery.address_3 || undefined;
+  console.log(typeof address);
   return (
     <div className='border border-black bg-white w-[23rem] h-72 rounded px-6 py-7 flex flex-col'>
-      <p className='text-gray-800 font-bold text-xl mb-5'>
-        10-56 Brewing Company
-      </p>
+      <p className='text-gray-800 font-bold text-xl mb-5'>{brewery.name}</p>
       <p className='w-full text-gray-700 h-14 mb-6'>
-        400 Brown Cir Knox, Indiana - United States
+        {typeof address == 'string' ? address : ''}
+        {brewery.state &&
+          `${typeof address == 'string' ? ', ' : ''} ${brewery.state}`}
+        {brewery.country && ` - ${brewery.country}`}
       </p>
       <div className='w-full flex flex-wrap gap-x-4 gap-y-7'>
-        <CardChip label={'micro'} icon={<ChartSquareBarIcon />} />
-        <CardChip label={'46534'} icon={<LocationMarkerIcon />} />
-        <CardChip label={'6308165790'} icon={<PhoneIcon />} />
+        {brewery.brewery_type && (
+          <CardChip
+            label={brewery.brewery_type}
+            icon={<ChartSquareBarIcon />}
+          />
+        )}
+        {brewery.postal_code && (
+          <CardChip label={brewery.postal_code} icon={<LocationMarkerIcon />} />
+        )}
+        {brewery.phone && (
+          <CardChip label={brewery.phone} icon={<PhoneIcon />} />
+        )}
         <CardChip label={'add more'} icon={<PlusCircleIcon />} />
       </div>
     </div>
--- pages/brewery-list.tsx
+++ pages/brewery-list.tsx
@@ -1,21 +1,33 @@
 import type { NextPage } from 'next';
 import Header from '../components/Molecules/Header';
 import BreweryCard from '../components/Organisms/BreweryCard';
+import { getApi } from '../services/api';
+import { Brewery } from '../services/breweries/breweries.interface';
 
-const BreweryListPage: NextPage = () => {
+interface BreweryListPageProps {
+  breweries: Brewery[];
+}
+
+const BreweryListPage: NextPage<BreweryListPageProps> = ({ breweries }) => {
   return (
-    <main className='bg-[#FFFEF0] w-screen h-screen flex flex-col items-center'>
+    <main className='bg-[#FFFEF0] w-full min-h-screen flex flex-col items-center'>
       <Header />
-      <div className='flex justify-center flex-wrap gap-12 mt-24'>
-        <BreweryCard />
-        <BreweryCard />
-        <BreweryCard />
-        <BreweryCard />
-        <BreweryCard />
-        <BreweryCard />
+      <div className='my-24 w-full h-full flex justify-center flex-wrap gap-12'>
+        {breweries?.map((brewery) => {
+          return <BreweryCard key={brewery.id} brewery={brewery} />;
+        })}
       </div>
     </main>
   );
 };
 
 export default BreweryListPage;
+
+export async function getServerSideProps() {
+  const api = getApi();
+  const response = await api.get<Brewery[]>('/breweries');
+  console.log(response.data);
+  return {
+    props: { breweries: response.data }, // will be passed to the page component as props
+  };
+}
